a7d4d26ca35b3f9468653db9acecd755ec211b05
Sairam <pvsairam@hotmail.com>
2025-04-25 22:17:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import schedule, time
+from config import USE_TWITTER, USE_FARCASTER
 from poster.generator import generate_post
 from poster.memory import already_posted, save_post
-from poster.farcaster import post_to_farcaster
 from poster.telegram import log_to_telegram
+from poster.farcaster import post_to_farcaster
+from poster.twitter import post_to_twitter
 from data.projects import get_random_project
 
 def job():
     project = get_random_project()
     text = generate_post(project)
     if already_posted(text): return
-    post_to_farcaster(text)
+    if USE_FARCASTER:
+        post_to_farcaster(text)
+    if USE_TWITTER:
+        post_to_twitter(text)
     log_to_telegram(text)
     save_post(text)
 
